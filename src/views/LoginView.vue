d501cbf15f87c5f8899a804856c31f067f078045
LeXiang-Peng <1175817624@qq.com>
2024-04-27 14:10:52 +0800

    vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -63,8 +63,6 @@ export default {
     }
     sessionStorage.setItem("id_card", "");
     sessionStorage.setItem("currentPath", "");
-    sessionStorage.setItem("userName", "");
-    sessionStorage.setItem("avatar_url", "");
   },
 };
 </script>
